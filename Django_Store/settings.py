2225beec7d6d2110c70ab02f4d2e8501585bc659
Masoud Jaafary <masood.jaafary@gmail.com>
2021-09-10 19:07:04 +0430

    connect to postgres

--- Django_Store/settings.py
+++ Django_Store/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Django_Store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DjangoStore',
+        'USER': 'postgres',
+        'PASSWORD': 'mj9353981766',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
